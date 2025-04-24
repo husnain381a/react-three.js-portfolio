46c9fef69282abbfbec755f084d48ddee6dccdca
Hunsain Mazhar <Mazhar@DESKTOP-S88HF4T>
2025-04-24 21:48:46 +0500

    Added a 3d model to the about section The model is a globe that rotates on its axis.

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Globe from 'react-globe.gl'
+import Button from '../components/Button'
 
 function About() {
   return (
@@ -22,6 +24,29 @@ function About() {
                     <div>
                         <p className='grid-headtext'>Tech Stack</p>
                         <p className='grid-subtext'>I specialize in JavaScript/TypeScript with a focus on React and Next.js ecosystems.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className='col-span-1 xl:row-span-3'>
+                <div className='grid-container'>
+                    <div className='rounded-3xl w-full sm:h-[326px] h-fit flex justify-center items-center'>
+                        <Globe
+                        height={326}
+                        width={326}
+                        backgroundColor='rgba(0, 0, 0, 0)'
+                        backgroundImageOpacity={0.5}
+                        showAtmosphere
+                        showGraticules
+                        globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
+                        bumpImageUrl="//unpkg.com/three-globe/example/img/earth-topology.png"
+                        // labelsData={[{ lat: 40, lng: -100, text: 'Rjieka, Croatia', color: 'white', size: 15 }]}
+                        />
+                    </div>
+                    <div>
+                    <p className="grid-headtext">I’m very flexible with time zone communications & locations</p>
+                    <p className="grid-subtext">I&apos;m based in Rjieka, Croatia and open to remote work worldwide.</p>
+                    <Button name="Contact Me" isBeam containerClass="w-full mt-10" />
                     </div>
                 </div>
             </div>
