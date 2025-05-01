89ea0a1ab25cc2b67ed46d458cd1a03d5935c09f
Hunsain Mazhar <Mazhar@DESKTOP-S88HF4T>
2025-05-01 22:33:42 +0500

    initial

--- src/constants/index.js
+++ src/constants/index.js
@@ -358,15 +358,15 @@ export const navLinks = [
   ];
   
   export const calculateSizes = (isSmall, isMobile, isTablet) => {
-  return {
-    deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
-    deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
-    cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
-    reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
-    ringPosition: isSmall ? [-2, 3, 0] : isMobile ? [-4, 3, 0] : isTablet ? [-8, 3, 0] : [-10, 3, 0],
-    targetPosition: isSmall ? [-5, -10, 1] : isMobile ? [-9, -10, 1] : isTablet ? [-11, -7, 1] : [-13, -13, 1],
+    return {
+      deskScale: isSmall ? 0.05 : isMobile ? 0.06 : 0.065,
+      deskPosition: isMobile ? [0.5, -4.5, 0] : [0.25, -5.5, 0],
+      cubePosition: isSmall ? [4, -5, 0] : isMobile ? [5, -5, 0] : isTablet ? [5, -5, 0] : [9, -5.5, 0],
+      reactLogoPosition: isSmall ? [3, 4, 0] : isMobile ? [5, 4, 0] : isTablet ? [5, 4, 0] : [12, 3, 0],
+      ringPosition: isSmall ? [-2, 3, 0] : isMobile ? [-4, 3, 0] : isTablet ? [-8, 3, 0] : [-10, 3, 0],
+      targetPosition: isSmall ? [-5, -10, -10] : isMobile ? [-9, -10, -10] : isTablet ? [-11, -7, -10] : [-13, -13, -10],
+    };
   };
-};
   
   export const workExperiences = [
     {
